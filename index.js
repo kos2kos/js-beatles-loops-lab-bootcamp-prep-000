4c12f4146942399fbe7a0c0338699156a437d1a2
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-08 01:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var temp = [];
   for (var i = 0; i < musicians.length; i++){
-   temp[i] = `${musicians[i]
-  }
+   temp[i] = `${musicians[i]} plays ${instruments[i]}`
+  }return temp
 }
